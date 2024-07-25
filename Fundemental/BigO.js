1b52e94acf6f750d8ca755385338f97c176f0bca
mustafataha5 <mustafa.taha.mu95@gmail.com>
2024-07-25 15:45:57 +0300

    how long

--- Fundemental/BigO.js
+++ Fundemental/BigO.js
@@ -41,10 +41,25 @@ while( primeCount < 1e4 ) {
     }
     num++;
 }
-console.log(`The 10,000th prime number is ${num-1}`);
+console.log(`The 10,000th prime number is ${num-1} with inhance`);
 console.log(`This took ${performance.now() - start1} milliseconds to run`);
 
+let start2 = performance.now();
+ primeCount = 0;
+ num = 2; // for math reasons, 1 is considered prime
+while( primeCount < 1e5 ) {
+    if( num.isPrime_inhance() ) {
+        primeCount++;
+    }
+    num++;
+}
+console.log(`The 100,000th prime number is ${num-1} with inhance`);
+console.log(`This took ${performance.now() - start2} milliseconds to run`);
+
 // recursive
+let start3 = performance.now();
+ primeCount = 0;
+
 function rFib( n ) {
         if ( n < 2 ) {
             return n;
@@ -52,7 +67,13 @@ function rFib( n ) {
         return rFib( n-1 ) + rFib( n-2 );
     }
     rFib(20);
+    console.log(`The Fibonacci with recursive`);
+console.log(`This took ${performance.now() - start3} milliseconds to run`);
+
+    
     // iterative
+    let start4= performance.now();
+ primeCount = 0;
     function iFib( n ) {
         const vals = [ 0, 1 ];
         while(vals.length-1 < n) {
@@ -62,7 +83,35 @@ function rFib( n ) {
         return vals[n];
     }
     iFib(20);
+    console.log(`The Fibonacci with iterative`);
+    console.log(`This took ${performance.now() - start4} milliseconds to run`);
     
+    
+    let start5= performance.now();
+    primeCount = 0;
     const story = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident culpa nihil repellat nulla laboriosam maxime, quia aliquam ipsam reprehenderit delectus reiciendis molestias assumenda aut fugit tempore laudantium tempora aspernatur? Repellendus consequatur expedita doloribus soluta cupiditate quae fugit! Aliquid, repellat animi, illum molestias maiores, laboriosam vero impedit iusto mollitia optio labore asperiores!";
     const reversed1 = story.split("").reverse().join("");
+    console.log(`The Reverse of String with built in function`);
+    console.log(`This took ${performance.now() - start5} milliseconds to run`);
+
+    let start6= performance.now();
+    primeCount = 0;
+    // const story1 = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident culpa nihil repellat nulla laboriosam maxime, quia aliquam ipsam reprehenderit delectus reiciendis molestias assumenda aut fugit tempore laudantium tempora aspernatur? Repellendus consequatur expedita doloribus soluta cupiditate quae fugit! Aliquid, repellat animi, illum molestias maiores, laboriosam vero impedit iusto mollitia optio labore asperiores!";
+    let arr = story.split('');
+    let left = 0;
+    let right = arr.length - 1;
     
+    while (left < right) {
+        // Swap characters
+        let temp = arr[left];
+        arr[left] = arr[right];
+        arr[right] = temp;
+        
+        left++;
+        right--;
+    }
+    
+    console.log(arr.join());
+    console.log(`The Reverse of String with My way function`);
+    console.log(`This took ${performance.now() - start6} milliseconds to run`);
+
