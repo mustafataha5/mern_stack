de3f457785dfe4d44fd413cea6ae54a8e9b1b0fd
mustafataha5 <mustafa.taha.mu95@gmail.com>
2024-07-31 16:16:57 +0300

    todo list

--- React/Vite/todo-list/src/App.jsx
+++ React/Vite/todo-list/src/App.jsx
@@ -1,15 +1,87 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 // import './App.css'
 import InputForm from './components/InputForm'
+import { useEffect, useRef } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [data, setData] = useState([])
+  const isInitialLoad = useRef(true)
+
+  
+  //load data 
+  useEffect(() => {
+    const newData = localStorage.getItem('tasks')
+    if (newData) {
+      console.log("new data ------" + newData);
+      setData(JSON.parse(newData));
+    }
+  }, []);
+
+  //save data on loaclstorge 
+  useEffect(() => {
+    if(isInitialLoad.current){
+        isInitialLoad.current = false ; 
+    }
+    else{
+      localStorage.setItem('tasks', JSON.stringify(data))
+      console.log("save data ------" + data);
+    }
+  }, [data])
+
+  const AddTask = (newdata) => {
+
+    setData([...data, newdata]);
+  };
+
+  const checkChange = (checked, index) => {
+    const newData = data.map((task, i) => index === i ? { ...task, complete: checked } : task);
+    //console.log(i + "------" + data.length + "  ---------" + checked)
+    //newData[i].complete = checked;
+    // console.log(i+"------"+data[i].complete)
+    setData(newData);
+  }
+
+  const deleteTask = (index) => {
+    setData(data.filter((_, i) => i !== index))
+  }
 
   return (
-    <>  
-        <InputForm ></InputForm>
+    <>
+      <InputForm onAdd={AddTask} ></InputForm>
+
+      <div className='row d-flex justify-content-center'>
+        <div className='col-md-6'>
+          <div className='card border-light '>
+            <h3 className='card-title text-center'>My Task:</h3>
+            <div className='card-body '>
+
+              <table className='table table-md '>
+                <tbody>
+
+                  {data.length > 0 && data.map((task, i) =>
+                    <tr className='' key={i} >
+                      <td className=' text-end'>
+                        {task.complete ? <p><del>{task.task}</del></p> : <p  >  {task.task} </p>}
+
+                      </td>
+
+                      <td>
+                        <input className='mb-4' type="checkbox" checked={task.complete} onChange={(e) => checkChange(e.target.checked, i)} />
+                      </td>
+                      <td>
+                        <button className='btn btn-sm btn-dark' onClick={() => deleteTask(i)}>Delete</button>
+                      </td>
+                    </tr>
+                  )
+                  }
+                </tbody>
+              </table>
+            </div>
+
+
+          </div>
+        </div>
+      </div>
     </>
   )
 }
--- React/Vite/todo-list/src/components/InputForm.jsx
+++ React/Vite/todo-list/src/components/InputForm.jsx
@@ -4,12 +4,16 @@ const InputForm = (props) => {
   
     const [value , setValue] = useState("") ; 
 
-  
-  
+    const addNewTask = (e) =>{
+        e.preventDefault(); 
+        props.onAdd({task:value,complete:false });
+        setValue('') ; 
+    } 
+   
     return (
     <>
-      <form >
-        <div className='d-flex justify-content-center my-5 rounded round-1'>
+      <form className='d-flex justify-content-center' onSubmit={addNewTask}>
+        <div className='d-flex justify-content-between w-30 px-5 py-2 my-5 border border-dark'>
             <input className='' type="text" value={value} onChange={(e) => {setValue(e.target.value)}} />
             <input className='mx-3 btn btn-outline-primary' type="submit" value="Add" />
         </div>
