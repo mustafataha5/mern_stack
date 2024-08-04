215f733e79cd430e62bc38084506abcd45965073
mustafataha5 <mustafa.taha.mu95@gmail.com>
2024-08-04 17:27:25 +0300

    Luke APIwalker

--- React/Vite/luke-apiwalker/src/components/People.jsx
+++ React/Vite/luke-apiwalker/src/components/People.jsx
@@ -8,15 +8,18 @@ import { Link, useParams } from 'react-router-dom';
 const People = () => {
    
     const [result,setResult] = useState() ;
+    const [loading, setLoading] = useState(true);
      const [peopleFrom,setPeopleFrom] = useState('');
     const {id} = useParams() ; 
     useEffect( ()=>{
       axios.get('https://swapi.dev/api/people/'+id) 
       .then(res => {setResult(res.data); 
+                    setLoading(false) ; 
       })
       .catch(err => {console.log("------------<<<<<"+err)
             setResult(undefined) ;
-            navigate(qurey)
+            setLoading(false) ; 
+            //navigate(qurey)
           });
 
     },[id])
@@ -28,27 +31,27 @@ const People = () => {
       }
     },[])
   
-  
-    return (
-    <>
-    
-    { (result ==undefined) ?  <ErrorMessage></ErrorMessage>:
-       ( <div>
-      <h2> {result.name} </h2>
-      <p><b> Height: </b> {result.height}</p>
-      <p><b> Mass:</b> {result.mass}</p>
-      <p><b> Hair Color:</b> {result.hair_color}</p>
-      <p><b> Skin Color:</b> {result.skin_color}</p>
+    if (loading) {
+      return <div>Loading...</div>; // You can replace this with a spinner or a more styled loading component
+  }
+
+  if (result===undefined) {
+      return <ErrorMessage />;
+  }
+
+  return (
       <div>
-       
-        <h4>From Planet:  <Link to={'/planets/'+result.homeworld.split('/').at(-2)}>Go to Planet</Link></h4>
-        {/* <Planets result={peopleFrom}></Planets> */}
+          <h2>{result.name}</h2>
+          <p><b>Height:</b> {result.height}</p>
+          <p><b>Mass:</b> {result.mass}</p>
+          <p><b>Hair Color:</b> {result.hair_color}</p>
+          <p><b>Skin Color:</b> {result.skin_color}</p>
+          <div>
+              <h4>From Planet: <Link to={`/planets/${result.homeworld.split('/').at(-2)}`}>Go to Planet</Link></h4>
+              {/* <Planets result={peopleFrom} /> */}
+          </div>
       </div>
-    </div>)
-    }
-    
-    </>
-  )
+  );
 }
 
 export default People
--- React/Vite/luke-apiwalker/src/components/Planets.jsx
+++ React/Vite/luke-apiwalker/src/components/Planets.jsx
@@ -4,31 +4,43 @@ import { useParams } from 'react-router-dom';
 import axios from 'axios';
 
 const Planets = () => {
-   // console.log(result)
-   const [result,setResult]= useState() ; 
-   const {id} = useParams() ; 
+  // console.log(result)
+  const [result, setResult] = useState();
+  const [loading, setLoading] = useState(true);
+  const { id } = useParams();
 
-  useEffect(()=>{
-      axios.get('https://swapi.dev/api/planets/'+id) 
-      .then(res => {setResult(res.data); 
+  useEffect(() => {
+    axios.get('https://swapi.dev/api/planets/' + id)
+      .then(res => {
+        setResult(res.data);
+        setLoading(false);
       })
-      .catch(err => {console.log("------------<<<<<"+err)
-            setResult(undefined) ;
-          });
-  },[id]);
+      .catch(err => {
+        console.log("------------<<<<<" + err)
+        setResult(undefined);
+      });
+  }, [id]);
 
-  return (
-    <>
-    {(result ==undefined) ?  <ErrorMessage></ErrorMessage>:
-    ( <div>
-       <h2> {result.name} </h2>
-       <p> <b>climate:</b> {result.climate} </p>
-       <p> <b>terrain:</b> {result.terrain} </p>
-       <p> <b>surface_water:</b> {result.surface_water} </p>
-       <p> <b>population:</b> {result.population} </p>
-      </div>)}
-    </>
-  )
+  if (loading) {
+    return <div>loading ... </div>
+  }
+  if (result === undefined) {
+    <ErrorMessage></ErrorMessage>
+  }
+  else {
+
+    return (
+      <>
+        <div>
+          <h2> {result.name} </h2>
+          <p> <b>climate:</b> {result.climate} </p>
+          <p> <b>terrain:</b> {result.terrain} </p>
+          <p> <b>surface_water:</b> {result.surface_water} </p>
+          <p> <b>population:</b> {result.population} </p>
+        </div>
+      </>
+    )
+  }
 }
 
 export default Planets
